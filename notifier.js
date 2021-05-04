961f2e9ae9a9c1964ab25bd2c89591f96a1ab212
root <root@APP-ROG.localdomain>
2021-05-04 10:43:48 +0530

    District Wise Fetch weekly Data fetch instead of daily

--- notifier.js
+++ notifier.js
@@ -1,25 +1,24 @@
-let nodemailer = require('nodemailer');
+let nodemailer = require("nodemailer");
 
 let nodemailerTransporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: String(process.env.EMAIL),
-        pass: String(process.env.APPLICATION_PASSWORD)
-    }
+  service: "Gmail",
+  auth: {
+    user: String(process.env.EMAIL),
+    pass: String(process.env.APPLICATION_PASSWORD),
+  },
 });
 
-
 exports.sendEmail = function (email, subjectLine, slotDetails, callback) {
-    let options = {
-        from: String('Vaccine Checker ' + process.env.EMAIL),
-        to: email,
-        subject: subjectLine,
-        text: 'Vaccine available. Details: \n\n' + slotDetails
-    };
-    nodemailerTransporter.sendMail(options, (error, info) => {
-        if (error) {
-            return callback(error);
-        }
-        callback(error, info);
-    });
+  let options = {
+    from: String("Vaccine Checker " + process.env.EMAIL),
+    to: email,
+    subject: subjectLine,
+    text: "Vaccine available. Details: \n\n" + slotDetails,
+  };
+  nodemailerTransporter.sendMail(options, (error, info) => {
+    if (error) {
+      return callback(error);
+    }
+    callback(error, info);
+  });
 };
--- vaccineNotifier.js
+++ vaccineNotifier.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const moment = require('moment');
-const cron = require('node-cron');
-const axios = require('axios');
-const notifier = require('./notifier');
+require("dotenv").config();
+const moment = require("moment");
+const cron = require("node-cron");
+const axios = require("axios");
+const notifier = require("./notifier");
 /**
 Step 1) Enable application access on your gmail with steps given here:
  https://support.google.com/accounts/answer/185833?p=InvalidSecondFactor&visit_id=637554658548216477-2576856839&rd=1
@@ -14,75 +14,88 @@ Step 3) On your terminal run: npm i && pm2 start vaccineNotifier.js
 To close the app, run: pm2 stop vaccineNotifier.js && pm2 delete vaccineNotifier.js
  */
 
-const PINCODE = process.env.PINCODE
-const EMAIL = process.env.EMAIL
-const AGE = process.env.AGE
+const PINCODES = process.env.PINCODES;
+const DISTRICT = process.env.DISTRICT;
+const EMAIL = process.env.EMAIL;
+const AGE = process.env.AGE;
 
-async function main(){
-    try {
-        cron.schedule('* * * * *', async () => {
-             await checkAvailability();
-        });
-    } catch (e) {
-        console.log('an error occured: ' + JSON.stringify(e, null, 2));
-        throw e;
-    }
+async function main() {
+  try {
+    cron.schedule("* * * * *", async () => {
+      await checkAvailability();
+    });
+  } catch (e) {
+    console.log("an error occured: " + JSON.stringify(e, null, 2));
+    throw e;
+  }
 }
 
 async function checkAvailability() {
-
-    let datesArray = await fetchNext10Days();
-    datesArray.forEach(date => {
-        getSlotsForDate(date);
-    })
+  let datesArray = await fetchNext10Days();
+  datesArray.forEach((date) => {
+    getSlotsForDate(date);
+  });
 }
 
 function getSlotsForDate(DATE) {
-    let config = {
-        method: 'get',
-        url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=' + PINCODE + '&date=' + DATE,
-        headers: {
-            'accept': 'application/json',
-            'Accept-Language': 'hi_IN'
-        }
-    };
+  let config = {
+    method: "get",
+    url:
+      "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" +
+      DISTRICT +
+      "&date=" +
+      DATE,
+    headers: {
+      accept: "application/json",
+      "Accept-Language": "hi_IN",
+    },
+  };
 
-    axios(config)
-        .then(function (slots) {
-            let sessions = slots.data.sessions;
-            let validSlots = sessions.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0)
-            console.log({date:DATE, validSlots: validSlots.length})
-            if(validSlots.length > 0) {
-                notifyMe(validSlots);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
+  axios(config)
+    .then(function (slots) {
+      let centers = slots.data.centers;
+      let validSlots = [];
+
+      centers.forEach(function (center) {
+        center.sessions.every(function (slot) {
+          if (slot.min_age_limit <= AGE && slot.available_capacity > 0) {
+            validSlots.push(center);
+            return false;
+          }
+          return true;
         });
-}
-
-async function
-
-notifyMe(validSlots){
-    let slotDetails = JSON.stringify(validSlots, null, '\t');
-    notifier.sendEmail(EMAIL, 'VACCINE AVAILABLE', slotDetails, (err, result) => {
-        if(err) {
-            console.error({err});
-        }
+      });
+      console.log({ date: DATE, validSlots: validSlots.length });
+      if (validSlots.length > 0) {
+        notifyMe(validSlots);
+      }
     })
-};
-
-async function fetchNext10Days(){
-    let dates = [];
-    let today = moment();
-    for(let i = 0 ; i < 10 ; i ++ ){
-        let dateString = today.format('DD-MM-YYYY')
-        dates.push(dateString);
-        today.add(1, 'day');
-    }
-    return dates;
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
+async function notifyMe(validSlots) {
+  let slotDetails = JSON.stringify(validSlots, null, "\t");
+  notifier.sendEmail(EMAIL, "VACCINE AVAILABLE", slotDetails, (err, result) => {
+    if (err) {
+      console.error({ err });
+    }
+  });
+}
 
-main()
-    .then(() => {console.log('Vaccine availability checker started.');});
+async function fetchNext10Days() {
+  let dates = [];
+  let today = moment();
+  for (let i = 0; i < 10; i++) {
+    let dateString = today.format("DD-MM-YYYY");
+    dates.push(dateString);
+    today.add(7, "day");
+  }
+  return dates;
+}
+
+main().then(() => {
+  console.log("Vaccine availability checker started.");
+  checkAvailability();
+});
